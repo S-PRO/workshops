412668a881de3692a24193d11404198fed6600d0
Alex Kazantsev <akazantsev@s-pro.io>
2017-05-29 12:11:36 +0300

    remove list issues

--- node-rest-api-1/app/middlewares/check-auth.js
+++ node-rest-api-1/app/middlewares/check-auth.js
@@ -1,9 +1,8 @@
 import Boom from 'boom';
 
-import { TokenService } from './../utils'
+import { TokenService } from './../utils';
 
 export default async (ctx, next) => {
-  console.log(ctx.request)
   const { request: { header: { authorization } } } = ctx;
   if (!TokenService.decode(authorization)) throw Boom.forbidden('You are not authorized!');
   await next();
